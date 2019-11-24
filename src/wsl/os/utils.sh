093bc749f232a1a45a7f943ab3bbf68b4b000254
NikiforovAll <nikiforovalekcey@gmail.com>
2019-11-24 15:43:41 +0200

    mvp for setup

--- src/wsl/os/utils.sh
+++ src/wsl/os/utils.sh
@@ -245,7 +245,45 @@ verify_os() {
     return 1
 
 }
+get_os() {
 
+    local os=""
+    local kernelName=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    kernelName="$(uname -s)"
+
+    if [ "$kernelName" == "Darwin" ]; then
+        os="macos"
+    elif [ "$kernelName" == "Linux" ] && \
+         [ -e "/etc/os-release" ]; then
+        os="$(. /etc/os-release; printf "%s" "$ID")"
+    else
+        os="$kernelName"
+    fi
+
+    printf "%s" "$os"
+
+}
+
+get_os_version() {
+
+    local os=""
+    local version=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    os="$(get_os)"
+
+    if [ "$os" == "macos" ]; then
+        version="$(sw_vers -productVersion)"
+    elif [ -e "/etc/os-release" ]; then
+        version="$(. /etc/os-release; printf "%s" "$VERSION_ID")"
+    fi
+
+    printf "%s" "$version"
+}
 answer_is_yes() {
     [[ "$REPLY" =~ ^[Yy]$ ]] \
         && return 0 \
