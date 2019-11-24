5a4b3f5da087b72769028ffa2c1e80e88dce99eb
NikiforovAll <nikiforovalekcey@gmail.com>
2019-11-24 15:43:41 +0200

    mvp for setup

--- src/wsl/os/install.sh
+++ src/wsl/os/install.sh
@@ -3,6 +3,7 @@
 declare -r GITHUB_REPOSITORY="nikiforovall/dotfiles"
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/src/wsl/os/utils.sh"
 declare dotfilesDirectory="$HOME/projects/dotfiles"
 declare skipQuestions=false
 # ----------------------------------------------------------------------
@@ -144,44 +145,19 @@ verify_os() {
 
     declare -r MINIMUM_MACOS_VERSION="10.10"
     declare -r MINIMUM_UBUNTU_VERSION="18.04"
-
     local os_name="$(get_os)"
     local os_version="$(get_os_version)"
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Check if the OS is `macOS` and
-    # it's above the required version.
-
-    if [ "$os_name" == "macos" ]; then
-
-        if is_supported_version "$os_version" "$MINIMUM_MACOS_VERSION"; then
-            return 0
-        else
-            printf "Sorry, this script is intended only for macOS %s+" "$MINIMUM_MACOS_VERSION"
-        fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Check if the OS is `Ubuntu` and
-    # it's above the required version.
-
-    elif [ "$os_name" == "ubuntu" ]; then
+    if [ "$os_name" == "ubuntu" ]; then
 
         if is_supported_version "$os_version" "$MINIMUM_UBUNTU_VERSION"; then
             return 0
         else
             printf "Sorry, this script is intended only for Ubuntu %s+" "$MINIMUM_UBUNTU_VERSION"
         fi
-
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    else
-        printf "Sorry, this script is intended only for macOS and Ubuntu!"
     fi
-
     return 1
-
 }
 # ----------------------------------------------------------------------
 # | Main                                                               |
@@ -195,10 +171,18 @@ main() {
     cd "$(dirname "${BASH_SOURCE[0]}")" \
         || exit 1
 
-    source ./utils.sh
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Load utils
 
+    if [ -x "utils.sh" ]; then
+        . "utils.sh" || exit 1
+    else
+        download_utils || exit 1
+    fi
+    # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    source ./utils.sh
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Ensure the OS is supported and
@@ -209,24 +193,20 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    skip_questions "$@" \
-        && skipQuestions=true
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     ask_for_sudo
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Check if this script was run directly (./<path>/setup.sh),
     # and if not, it most likely means that the dotfiles were not
     # yet set up, and they will need to be downloaded.
-
+    # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     printf "%s" "${BASH_SOURCE[0]}" | grep "install.sh" &> /dev/null \
       || download_dotfiles
-    # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 
     ./create_directories.sh
+
 
     # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
